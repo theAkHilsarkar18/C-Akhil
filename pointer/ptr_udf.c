4478426f9b991f44814c9f1255801cbaac039fb3
theakhilsarkar18 <akhilsodvadiya7@gmail.com>
2023-09-10 20:12:22 +0530

    10/09/2023

--- pointer/ptr_udf.c
+++ pointer/ptr_udf.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 
 // function declaration
+
 // call by value function
 void square(int a, int b);
 // call by reference function
 void _square();
+
 int main()
 {
     int a = 2;
     int b = 4;
     square(a, b);
+    _square(&a, &b);
+    printf("a = %d, b = %d\n", a, b);
 }
 
 void square(int a, int b)
@@ -17,4 +21,11 @@ void square(int a, int b)
     a = a * a;
     b = b * b;
     printf("a = %d, b = %d\n", a, b);
+}
+
+void _square(int *a, int *b)
+{
+    *a = *a * *b;
+    *b = *b * *b;
+    printf("a = %d, b = %d\n", *a, *b);
 }
