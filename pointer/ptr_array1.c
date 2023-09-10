1620b316f4c3b7320bf10ce90b2c52fc281b909d
theakhilsarkar18 <akhilsodvadiya7@gmail.com>
2023-09-10 17:21:47 +0530

    10/10/2023

---
+++ pointer/ptr_array1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    // Q.1 Write a Program to find the square of each element of a given 1D array using a Pointer.
+    // For example,
+    // Input:
+    // Enter the array's size: 5
+
+    // Enter array's elements:
+    // a[0] = 2
+    // a[1] = 4
+    // a[2] = 1
+    // a[3] = 3
+    // a[4] = 7
+
+    // Output:
+    // Square of each element:
+    // 4, 16, 1, 9, 49
+    int array[5] = {2, 3, 4, 5};
+    
+
+}
