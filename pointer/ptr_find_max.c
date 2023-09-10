6980f8a48712d994e36058f1a93fb82f1ef024b9
theakhilsarkar18 <akhilsodvadiya7@gmail.com>
2023-09-10 20:17:45 +0530

    10/09/2023

--- pointer/ptr_find_max.c
+++ pointer/ptr_find_max.c
@@ -1,5 +1,35 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
     // wap to find maximum number using pointer.
+    int a = 10;
+    int b = -30;
+    int c = 0;
+
+    int *ptr1 = &a;
+    int *ptr2 = &b;
+    int *ptr3 = &c;
+
+    if (*ptr1 > *ptr2)
+    {
+        if (*ptr1 > *ptr3)
+        {
+            printf("%d is maximum", *ptr1);
+        }
+        else
+        {
+            printf("%d is maximum", *ptr2);
+        }
+    }
+    else
+    {
+        if (*ptr2 > *ptr3)
+        {
+            printf("%d is maximum", *ptr2);
+        }
+        else
+        {
+            printf("%d is maximum", *ptr3);
+        }
+    }
 }
