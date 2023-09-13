d5f70ca9a74012702ba9eb0696fb42a05cf628a8
akhil <akhilsodvadiya7@gmail.com>
2023-09-13 19:12:49 +0530

    13-09-2023

---
+++ ArrayADV/acending.c
@@ -0,0 +1,26 @@
+// WAP to get the array and print in acending order.
+#include<stdio.h>
+int main()
+{
+    // arrange this array in acending : low to high
+    int a[10] = {1,2,4,6,7,3,4,6,8,9};
+    int temp = 0;
+
+    for(int i=0; i<10; i++)
+    {
+        for(int j=i+1; j<10; j++)
+        {
+            if(a[i]>a[j])
+            {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+
+    for(int i=0; i<10; i++)
+    {
+        printf("%d ",a[i]);
+    }
+}
