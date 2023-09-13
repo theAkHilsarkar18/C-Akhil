d53976c00af95f05a1b57b6a9675ae9d69c582b1
akhil <akhilsodvadiya7@gmail.com>
2023-09-13 19:15:57 +0530

    13-09-2023

---
+++ ArrayADV/frequency.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    // 3. Write a program in C to count the frequency of each element of an array.
+    int a[10] = {1,2,3,4,2,4,2,5,7,1};
+    // check how many number repeats how many times.
+    
+
+    for(int i=0; i<10; i++)
+    {
+        for(int j=i+1; j<10; j++)
+        {
+            if(a[i]==a[j])
+            {
+                
+            }
+        }
+    }
+
+}
