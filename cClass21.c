f6f8f62330a8b371492c18ccec29383c26295207
akhil <akhilsodvadiya7@gmail.com>
2023-10-07 16:25:38 +0530

    07-10-2023

--- cClass21.c
+++ cClass21.c
@@ -1,22 +1,22 @@
 
 
-#include<stdio.h>
+// #include<stdio.h>
 
-int main()
-{
-	int a[2][3] = {{1,2,3},{1,2,3}};
-	int sum = 0;
-	int b[3][3] = {{1,2,3},{1,2,3},{1,2,3}};
+// int main()
+// {
+// 	int a[2][3] = {{1,2,3},{1,2,3}};
+// 	int sum = 0;
+// 	int b[3][3] = {{1,2,3},{1,2,3},{1,2,3}};
 	
-	for(int i=0; i<3; i++)
-	{
-		for(int j=0; j<3; j++)
-		{
-			printf("enter the value : ");
-			scanf("%d",&b[i][j]);
-		}
+// 	for(int i=0; i<3; i++)
+// 	{
+// 		for(int j=0; j<3; j++)
+// 		{
+// 			printf("enter the value : ");
+// 			scanf("%d",&b[i][j]);
+// 		}
 		
-	}
+// 	}
 
 	// int x = 2;
 	// for(int i=0; i<3; i++)
@@ -25,9 +25,9 @@ int main()
 	// 	x--;
 	// }
 
-	printf("sum is %d",sum);
+// 	printf("sum is %d",sum);
 
-}
+// }
 
 
 
