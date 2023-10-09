562665a492bdf8921679b15e19defcee21a74361
theakhilsarkar18 <akhilsodvadiya7@gmail.com>
2023-10-09 22:30:00 +0530

    09-10-2023

--- ArrayADV/frequency.c
+++ ArrayADV/frequency.c
@@ -5,21 +5,22 @@ int main()
     // check how many number repeats how many times in this array.
 
     int a[10] = {1, 2, 3, 4, 2, 4, 2, 5, 7, 1};
-    int f[10] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+    int count;
 
     for (int i = 0; i < 10; i++)
     {
+        count = 1;
         for (int j = i + 1; j < 10; j++)
         {
             if (a[i] == a[j])
             {
-                f[i]++;
+                count++;
+                a[j] = -1;
             }
         }
-    }
-
-    for (int i = 0; i < 10; i++)
-    {
-        printf("%d ", f[i]);
+        if (a[i] != -1)
+        {
+            printf("%d -> %d\n", a[i], count);
+        }
     }
 }
