a37d447c00c4e060f67036e64b3784e9e13077df
akhil <akhilsodvadiya7@gmail.com>
2023-10-23 12:34:28 +0530

    23-10-2023

--- pointer/ptr_length_string.c
+++ pointer/ptr_length_string.c
@@ -4,11 +4,12 @@ int main()
     // wap to find length of string using ptr
 
     char name[10] = "Red&White";
-    int *ptr = name[0];
+    char *ptr = name;
     int count = 0;
-
-    for(int i=0; name[i]!='\0'; i++)
+    while(*ptr!='\0')
     {
-        
+        count++;
     }
+    printf("%d",count);
+    return 0;
 }
