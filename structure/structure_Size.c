9fef698f08a3d2a8d4d365d1dc8e0962c2aba3be
akhil <akhilsodvadiya7@gmail.com>
2023-10-26 10:02:35 +0530

    26-10-2023

--- structure/structure_Size.c
+++ structure/structure_Size.c
@@ -2,22 +2,40 @@
 // size of struct 
 #include <stdio.h> 
 // #pragma pack(1)
-struct A { 
+union A { 
 
-    char name[10];//12
+    
     char y[10];//4
     int z;//4
-    
-    
-    
+    char name[10];//12
 		
 }s1; 
+
+enum week
+{
+    monday = 10,
+    t = 20,
+    x = 20,
+    y = 20,
+    z = 20,
+};
 
 int main() 
 { 
 
-	
-	printf("Size of struct: %ld", sizeof(s1)); 
+    char name[10];
+    printf("enter your name : ");
+    scanf("%[^\n]",name);
+
+    printf("%s",name);
+
+
+    // enum week day;
+    // day = monday;
+    // printf("%d",day);
+
+	// printf("\n\n");
+	// printf("Size of struct: %ld", sizeof(day)); 
 
 	return 0; 
 } 
