78043c8b93e1fc47d3ac970b46f7371916225f63
akhil <akhilsodvadiya7@gmail.com>
2023-12-28 13:56:13 +0530

    28-12-2023

---
+++ udf/udf_rocky.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+int mantri()
+{
+    int gunPart1 = 10;
+    return gunPart1;
+}
+
+int waiter()
+{
+    int gunPart2 = 10;
+    return gunPart2;
+}
+
+int oldage()
+{
+    int gunPart3 = 20;
+    return gunPart3;
+}
+
+int dealer(int gunPart1,int gunPart2, int gunPart3)
+{
+    int semiGun = 50 + gunPart1 + gunPart2 + gunPart3;
+    return semiGun;
+}
+
+int rocky()
+{
+    int gun = 10 + dealer(mantri(),waiter(),oldage());
+    return gun;
+}
+
+
+int main()
+{
+    printf("Garuda <------ Gun %d",rocky());
+    return 0;
+}
